6ca25beb9578ee42f4c7ffbf127c24ac6cd8bdad
Suchipi Izumi <me@suchipi.com>
2018-03-25 02:29:15 -0600

    Remove unnecessary stuff

--- karma.conf.js
+++ karma.conf.js
@@ -3,22 +3,10 @@ const path = require("path");
 module.exports = config => {
   config.set({
     basePath: __dirname,
-    plugins: ["karma-nightmare", "karma-jasmine", "karma-webpack"],
+    plugins: ["karma-nightmare", "karma-jasmine"],
     frameworks: ["jasmine"],
     browsers: ["Nightmare"],
     files: [
-      {
-        pattern: path.resolve(
-          __dirname,
-          "node_modules",
-          "babel-polyfill",
-          "dist",
-          "polyfill.js"
-        ),
-        watched: false,
-        included: true,
-        served: true
-      },
       {
         pattern: "index.test.js",
         watched: true,
@@ -26,32 +14,9 @@ module.exports = config => {
         included: true
       }
     ],
-    preprocessors: {
-      "index.test.js": ["webpack"]
-    },
-    webpack: {
-      devtool: "cheap-module-eval-source-map",
-      module: {
-        loaders: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            query: {
-              presets: [
-                [
-                  "babel-preset-env",
-                  {
-                    targets: { browsers: "last 2 Chrome versions" },
-                    modules: false
-                  }
-                ]
-              ],
-              plugins: ["transform-object-rest-spread"]
-            }
-          }
-        ]
-      }
+    nightmareOptions: {
+      show: true,
+      openDevTools: true
     }
   });
 };
